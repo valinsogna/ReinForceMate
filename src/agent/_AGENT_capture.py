4eaa2114e577ec7806957605d5c665ac36304a06
Valeria Insogna <valinsogna@gmail.com>
2023-07-11 19:07:33 +0200

    agent + env capture translated

--- src/agent/_AGENT_capture.py
+++ src/agent/_AGENT_capture.py
@@ -1,31 +1,17 @@
-from keras.models import Model, clone_model
-from keras.layers import Input, Conv2D, Dense, Reshape, Dot, Activation, Multiply
-from keras.optimizers import SGD
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
-import keras.backend as K
-
 
 def policy_gradient_loss(Returns):
-    def modified_crossentropy(action, action_probs):
-        cost = (K.categorical_crossentropy(action, action_probs, from_logits=False, axis=1) * Returns)
-        return K.mean(cost)
+    def modified_crossentropy(action_probs, action):
+        cost = (torch.nn.functional.cross_entropy(action_probs, action, reduction='none') * Returns)
+        return torch.mean(cost)
 
     return modified_crossentropy
 
-
-class Agent(object):
-
+class Agent:
     def __init__(self, gamma=0.5, network='linear', lr=0.01, verbose=0):
-        """
-        Agent that plays the white pieces in capture chess
-        Args:
-            gamma: float
-                Temporal discount factor
-            network: str
-                'linear' or 'conv'
-            lr: float
-                Learning rate, ideally around 0.1
-        """
         self.gamma = gamma
         self.network = network
         self.lr = lr
@@ -35,11 +21,6 @@ class Agent(object):
         self.long_term_mean = []
 
     def init_network(self):
-        """
-        Initialize the network
-        Returns:
-
-        """
         if self.network == 'linear':
             self.init_linear_network()
         elif self.network == 'conv':
@@ -48,146 +29,70 @@ class Agent(object):
             self.init_conv_pg()
 
     def fix_model(self):
-        """
-        The fixed model is the model used for bootstrapping
-        Returns:
-        """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
-        self.fixed_model = clone_model(self.model)
-        self.fixed_model.compile(optimizer=optimizer, loss='mse', metrics=['mae'])
-        self.fixed_model.set_weights(self.model.get_weights())
+        self.fixed_model = self.model.clone()
 
     def init_linear_network(self):
-        """
-        Initialize a linear neural network
-        Returns:
-
-        """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
-        input_layer = Input(shape=(8, 8, 8), name='board_layer')
-        reshape_input = Reshape((512,))(input_layer)
-        output_layer = Dense(4096)(reshape_input)
-        self.model = Model(inputs=[input_layer], outputs=[output_layer])
-        self.model.compile(optimizer=optimizer, loss='mse', metrics=['mae'])
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(8*8*8, 4096)
+        )
+        self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
 
     def init_conv_network(self):
-        """
-        Initialize a convolutional neural network
-        Returns:
-
-        """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
-        input_layer = Input(shape=(8, 8, 8), name='board_layer')
-        inter_layer_1 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
-        inter_layer_2 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
-        flat_1 = Reshape(target_shape=(1, 64))(inter_layer_1)
-        flat_2 = Reshape(target_shape=(1, 64))(inter_layer_2)
-        output_dot_layer = Dot(axes=1)([flat_1, flat_2])
-        output_layer = Reshape(target_shape=(4096,))(output_dot_layer)
-        self.model = Model(inputs=[input_layer], outputs=[output_layer])
-        self.model.compile(optimizer=optimizer, loss='mse', metrics=['mae'])
+        self.model = nn.Sequential(
+            nn.Conv2d(8, 1, kernel_size=1),
+            nn.Flatten(),
+            nn.Linear(64, 4096)
+        )
+        self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
 
     def init_conv_pg(self):
-        """
-        Convnet net for policy gradients
-        Returns:
-
-        """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
-        input_layer = Input(shape=(8, 8, 8), name='board_layer')
-        R = Input(shape=(1,), name='Rewards')
-        legal_moves = Input(shape=(4096,), name='legal_move_mask')
-        inter_layer_1 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
-        inter_layer_2 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
-        flat_1 = Reshape(target_shape=(1, 64))(inter_layer_1)
-        flat_2 = Reshape(target_shape=(1, 64))(inter_layer_2)
-        output_dot_layer = Dot(axes=1)([flat_1, flat_2])
-        output_layer = Reshape(target_shape=(4096,))(output_dot_layer)
-        softmax_layer = Activation('softmax')(output_layer)
-        legal_softmax_layer = Multiply()([legal_moves, softmax_layer])  # Select legal moves
-        self.model = Model(inputs=[input_layer, R, legal_moves], outputs=[legal_softmax_layer])
-        self.model.compile(optimizer=optimizer, loss=policy_gradient_loss(R))
+        self.model = nn.Sequential(
+            nn.Conv2d(8, 1, kernel_size=1),
+            nn.Flatten(),
+            nn.Linear(64, 4096),
+            nn.Softmax(dim=1)
+        )
+        self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
 
     def network_update(self, minibatch):
-        """
-        Update the Q-network using samples from the minibatch
-        Args:
-            minibatch: list
-                The minibatch contains the states, moves, rewards and new states.
+        states, moves, rewards, new_states = zip(*minibatch)
 
-        Returns:
-            td_errors: np.array
-                array of temporal difference errors
+        states = torch.stack(states)
+        moves = torch.tensor(moves)
+        rewards = torch.tensor(rewards)
+        new_states = torch.stack(new_states)
 
-        """
+        q_target = rewards + self.gamma * self.fixed_model(new_states).max(dim=1).values
 
-        # Prepare separate lists
-        states, moves, rewards, new_states = [], [], [], []
-        td_errors = []
-        episode_ends = []
-        for sample in minibatch:
-            states.append(sample[0])
-            moves.append(sample[1])
-            rewards.append(sample[2])
-            new_states.append(sample[3])
+        q_state = self.model(states)
+        q_state = q_state.view(len(minibatch), 64, 64)
 
-            # Episode end detection
-            if np.array_equal(sample[3], sample[3] * 0):
-                episode_ends.append(0)
-            else:
-                episode_ends.append(1)
+        td_errors = q_state.gather(1, moves.unsqueeze(1)).squeeze(1) - q_target
+        q_state.scatter_(1, moves.unsqueeze(1), q_target.unsqueeze(1))
 
-        # The Q target
-        q_target = np.array(rewards) + np.array(episode_ends) * self.gamma * np.max(
-            self.fixed_model.predict(np.stack(new_states, axis=0)), axis=1)
+        q_state = q_state.view(len(minibatch), 4096)
 
-        # The Q value for the remaining actions
-        q_state = self.model.predict(np.stack(states, axis=0))  # batch x 64 x 64
+        self.optimizer.zero_grad()
+        loss = nn.MSELoss()(self.model(states), q_state)
+        loss.backward()
+        self.optimizer.step()
 
-        # Combine the Q target with the other Q values.
-        q_state = np.reshape(q_state, (len(minibatch), 64, 64))
-        for idx, move in enumerate(moves):
-            td_errors.append(q_state[idx, move[0], move[1]] - q_target[idx])
-            q_state[idx, move[0], move[1]] = q_target[idx]
-        q_state = np.reshape(q_state, (len(minibatch), 4096))
-
-        # Perform a step of minibatch Gradient Descent.
-        self.model.fit(x=np.stack(states, axis=0), y=q_state, epochs=1, verbose=0)
-
-        return td_errors
+        return td_errors.detach().numpy()
 
     def get_action_values(self, state):
-        """
-        Get action values of a state
-        Args:
-            state: np.ndarray with shape (8,8,8)
-                layer_board representation
-
-        Returns:
-            action values
-
-        """
-        return self.fixed_model.predict(state) + np.random.randn() * 1e-9
+        state = torch.unsqueeze(state, 0)
+        return self.fixed_model(state).detach().numpy() + np.random.randn() * 1e-9
 
     def policy_gradient_update(self, states, actions, rewards, action_spaces, actor_critic=False):
-        """
-        Update parameters with Monte Carlo Policy Gradient algorithm
-        Args:
-            states: (list of tuples) state sequence in episode
-            actions: action sequence in episode
-            rewards: rewards sequence in episode
-
-        Returns:
-
-        """
         n_steps = len(states)
         Returns = []
-        targets = np.zeros((n_steps, 64, 64))
+        targets = torch.zeros((n_steps, 64, 64))
         for t in range(n_steps):
             action = actions[t]
             targets[t, action[0], action[1]] = 1
             if actor_critic:
-                R = rewards[t, action[0] * 64 + action[1]]
+                R = rewards[t][action[0] * 64 + action[1]]
             else:
                 R = np.sum([r * self.gamma ** i for i, r in enumerate(rewards[t:])])
             Returns.append(R)
@@ -198,13 +103,111 @@ class Agent(object):
             train_returns = np.stack(Returns, axis=0) - np.mean(self.long_term_mean)
         else:
             train_returns = np.stack(Returns, axis=0)
-        # print(train_returns.shape)
-        targets = targets.reshape((n_steps, 4096))
-        self.weight_memory.append(self.model.get_weights())
-        self.model.fit(x=[np.stack(states, axis=0),
-                          train_returns,
-                          np.concatenate(action_spaces, axis=0)
-                          ],
-                       y=[np.stack(targets, axis=0)],
-                       verbose=self.verbose
-                       )
+
+        targets = targets.view(n_steps, 4096)
+
+        self.weight_memory.append(self.model.state_dict())
+        self.optimizer.zero_grad()
+        loss = policy_gradient_loss(torch.tensor(train_returns))(self.model(states), targets)
+        loss.backward()
+        self.optimizer.step()
+
+## This is an optimized version of the above class
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
+# import numpy as np
+
+
+# def policy_gradient_loss(Returns):
+#     def modified_crossentropy(action_probs, action):
+#         cost = (nn.functional.cross_entropy(action_probs, action, reduction='none') * Returns)
+#         return torch.mean(cost)
+
+#     return modified_crossentropy
+
+
+# class Agent:
+#     def __init__(self, gamma=0.5, network='linear', lr=0.01, verbose=0):
+#         self.gamma = gamma
+#         self.network = network
+#         self.lr = lr
+#         self.verbose = verbose
+#         self.init_network()
+#         self.init_target_network()
+#         self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
+
+#     def init_network(self):
+#         if self.network == 'linear':
+#             self.model = nn.Linear(8 * 8 * 8, 4096)
+#         elif self.network == 'conv':
+#             self.model = nn.Sequential(
+#                 nn.Conv2d(8, 1, kernel_size=1),
+#                 nn.Flatten(),
+#                 nn.Linear(64, 4096)
+#             )
+#         elif self.network == 'conv_pg':
+#             self.model = nn.Sequential(
+#                 nn.Conv2d(8, 1, kernel_size=1),
+#                 nn.Flatten(),
+#                 nn.Linear(64, 4096),
+#                 nn.Softmax(dim=1)
+#             )
+
+#     def init_target_network(self):
+#         self.target_model = self.model.clone().eval()
+
+#     def network_update(self, minibatch):
+#         states, moves, rewards, new_states = zip(*minibatch)
+
+#         states = torch.stack(states)
+#         moves = torch.tensor(moves)
+#         rewards = torch.tensor(rewards)
+#         new_states = torch.stack(new_states)
+
+#         q_target = rewards + self.gamma * self.target_model(new_states).max(dim=1).values
+
+#         q_state = self.model(states)
+#         q_state = q_state.view(len(minibatch), 64, 64)
+
+#         td_errors = q_state.gather(1, moves.unsqueeze(1)).squeeze(1) - q_target
+
+#         q_state.scatter_(1, moves.unsqueeze(1), q_target.unsqueeze(1))
+#         q_state = q_state.view(len(minibatch), 4096)
+
+#         self.optimizer.zero_grad()
+#         loss = nn.MSELoss()(self.model(states), q_state)
+#         loss.backward()
+#         self.optimizer.step()
+
+#         return td_errors.detach().numpy()
+
+#     def get_action_values(self, state):
+#         state = torch.unsqueeze(state, 0)
+#         return self.target_model(state).detach().cpu().numpy() + np.random.randn() * 1e-9
+
+#     def policy_gradient_update(self, states, actions, rewards, action_spaces, actor_critic=False):
+#         n_steps = len(states)
+#         Returns = []
+#         targets = torch.zeros((n_steps, 64, 64))
+#         for t in range(n_steps):
+#             action = actions[t]
+#             targets[t, action[0], action[1]] = 1
+#             if actor_critic:
+#                 R = rewards[t][action[0] * 64 + action[1]]
+#             else:
+#                 R = np.sum([r * self.gamma ** i for i, r in enumerate(rewards[t:])])
+#             Returns.append(R)
+
+#         if not actor_critic:
+#             mean_return = np.mean(Returns)
+#             train_returns = np.stack(Returns, axis=0) - mean_return
+#         else:
+#             train_returns = np.stack(Returns, axis=0)
+
+#         targets = targets.view(n_steps, 4096)
+
+#         self.optimizer.zero_grad()
+#         loss = policy_gradient_loss(torch.tensor(train_returns))(self.model(states), targets)
+#         loss.backward()
+#         self.optimizer.step()
--- src/environment/_ENV_capture.py
+++ src/environment/_ENV_capture.py
@@ -22,16 +22,16 @@ It is mostly based on the Board object from python-chess. Some modifications are
 - White pieces have the value 1, black pieces are -1
 """
 
+import torch
 import chess
-import numpy as np
 
 mapper = {}
-mapper["p"] = 0 # pawn
-mapper["r"] = 1 # rook
-mapper["n"] = 2 # knight
-mapper["b"] = 3 # bishop
-mapper["q"] = 4 # queen
-mapper["k"] = 5 # king
+mapper["p"] = 0  # pawn
+mapper["r"] = 1  # rook
+mapper["n"] = 2  # knight
+mapper["b"] = 3  # bishop
+mapper["q"] = 4  # queen
+mapper["k"] = 5  # king
 mapper["P"] = 0
 mapper["R"] = 1
 mapper["N"] = 2
@@ -40,8 +40,7 @@ mapper["Q"] = 4
 mapper["K"] = 5
 
 
-class Board(object):
-
+class Board:
     def __init__(self, FEN=None):
         """
         Chess Board Environment
@@ -52,29 +51,27 @@ class Board(object):
         self.FEN = FEN
         self.board = chess.Board(self.FEN) if self.FEN else chess.Board()
         self.init_action_space()
-        self.layer_board = np.zeros(shape=(8, 8, 8))
+        self.layer_board = torch.zeros(8, 8, 8)
         self.init_layer_board()
 
     def init_action_space(self):
         """
         Initialize the action space
         Returns:
-
         """
-        self.action_space = np.zeros(shape=(64, 64))
+        self.action_space = torch.zeros(64, 64)
 
     def init_layer_board(self):
         """
-        Initalize the numerical representation of the environment
+        Initialize the numerical representation of the environment
         Returns:
-
         """
-        self.layer_board = np.zeros(shape=(8, 8, 8))
+        self.layer_board = torch.zeros(8, 8, 8)
         for i in range(64):
             row = i // 8
             col = i % 8
             piece = self.board.piece_at(i)
-            if piece == None:
+            if piece is None:
                 continue
             elif piece.symbol().isupper():
                 sign = 1
@@ -93,9 +90,8 @@ class Board(object):
         Args:
             action: tuple of 2 integers
                 Move from, Move to
-
         Returns:
-            epsiode end: Boolean
+            episode_end: bool
                 Whether the episode has ended
             reward: int
                 Difference in material value after the move
@@ -127,20 +123,21 @@ class Board(object):
     def get_random_action(self):
         """
         Sample a random action
-        Returns: move
-            A legal python chess move.
-
+        Returns:
+            move: chess.Move
+                A legal python chess move.
         """
         legal_moves = [x for x in self.board.generate_legal_moves()]
-        legal_moves = np.random.choice(legal_moves)
+        legal_moves = torch.choice(legal_moves)
         return legal_moves
 
     def project_legal_moves(self):
         """
         Create a mask of legal actions
-        Returns: np.ndarray with shape (64,64)
+        Returns:
+            torch.Tensor with shape (64, 64)
         """
-        self.action_space = np.zeros(shape=(64, 64))
+        self.action_space = torch.zeros(64, 64)
         moves = [[x.from_square, x.to_square] for x in self.board.generate_legal_moves()]
         for move in moves:
             self.action_space[move[0], move[1]] = 1
@@ -149,20 +146,157 @@ class Board(object):
     def get_material_value(self):
         """
         Sums up the material balance using Reinfield values
-        Returns: The material balance on the board
+        Returns:
+            The material balance on the board
         """
-        pawns = 1 * np.sum(self.layer_board[0, :, :])
-        rooks = 5 * np.sum(self.layer_board[1, :, :])
-        minor = 3 * np.sum(self.layer_board[2:4, :, :])
-        queen = 9 * np.sum(self.layer_board[4, :, :])
+        pawns = 1 * torch.sum(self.layer_board[0, :, :])
+        rooks = 5 * torch.sum(self.layer_board[1, :, :])
+        minor = 3 * torch.sum(self.layer_board[2:4, :, :])
+        queen = 9 * torch.sum(self.layer_board[4, :, :])
         return pawns + rooks + minor + queen
 
     def reset(self):
         """
         Reset the environment
         Returns:
-
         """
         self.board = chess.Board(self.FEN) if self.FEN else chess.Board()
         self.init_layer_board()
         self.init_action_space()
+
+
+# This is an optimize version of the Board class:
+# import torch
+# import chess
+
+# mapper = {
+#     "p": 0,  # pawn
+#     "r": 1,  # rook
+#     "n": 2,  # knight
+#     "b": 3,  # bishop
+#     "q": 4,  # queen
+#     "k": 5,  # king
+#     "P": 0,
+#     "R": 1,
+#     "N": 2,
+#     "B": 3,
+#     "Q": 4,
+#     "K": 5
+# }
+
+
+# class Board:
+#     def __init__(self, FEN: str = None):
+#         """
+#         Chess Board Environment
+#         Args:
+#             FEN: str
+#                 Starting FEN notation, if None then start in the default chess position
+#         """
+#         self.FEN = FEN
+#         self.board = chess.Board.from_board(chess.Board(self.FEN)) if self.FEN else chess.Board()
+#         self.init_action_space()
+#         self.layer_board = torch.zeros(8, 8, 8)
+#         self.init_layer_board()
+
+#     def init_action_space(self):
+#         """
+#         Initialize the action space
+#         """
+#         self.action_space = torch.zeros(64, 64)
+
+#     def init_layer_board(self):
+#         """
+#         Initialize the numerical representation of the environment
+#         """
+#         self.layer_board = torch.zeros(8, 8, 8)
+#         for square in chess.SQUARES:
+#             row, col = chess.square_rank(square), chess.square_file(square)
+#             piece = self.board.piece_at(square)
+#             if piece is None:
+#                 continue
+#             sign = 1 if piece.symbol().isupper() else -1
+#             layer = mapper[piece.symbol()]
+#             self.layer_board[layer, row, col] = sign
+#         if self.board.turn:
+#             self.layer_board[6] = 1 / self.board.fullmove_number
+#         if self.board.can_claim_draw():
+#             self.layer_board[7] = 1
+
+#     def step(self, action: chess.Move) -> tuple[bool, int]:
+#         """
+#         Run a step
+#         Args:
+#             action: chess.Move
+#                 Move from, Move to
+#         Returns:
+#             episode_end: bool
+#                 Whether the episode has ended
+#             reward: int
+#                 Difference in material value after the move
+#         """
+#         piece_balance_before = self.get_material_value()
+#         self.board.push(action)
+#         self.init_layer_board()
+#         piece_balance_after = self.get_material_value()
+#         if self.board.result() == "*":
+#             opponent_move = self.get_random_action()
+#             self.board.push(opponent_move)
+#             self.init_layer_board()
+#             capture_reward = piece_balance_after - piece_balance_before
+#             if self.board.result() == "*":
+#                 reward = 0 + capture_reward
+#                 episode_end = False
+#             else:
+#                 reward = 0 + capture_reward
+#                 episode_end = True
+#         else:
+#             capture_reward = piece_balance_after - piece_balance_before
+#             reward = 0 + capture_reward
+#             episode_end = True
+#         if self.board.is_game_over():
+#             reward = 0
+#             episode_end = True
+#         return episode_end, reward
+
+#     def get_random_action(self) -> chess.Move:
+#         """
+#         Sample a random action
+#         Returns:
+#             move: chess.Move
+#                 A legal python chess move.
+#         """
+#         legal_moves = list(self.board.generate_legal_moves())
+#         return torch.choice(legal_moves)
+
+#     def project_legal_moves(self) -> torch.Tensor:
+#         """
+#         Create a mask of legal actions
+#         Returns:
+#             torch.Tensor with shape (64, 64)
+#         """
+#         self.action_space = torch.zeros_like(self.action_space)
+#         moves = [[move.from_square, move.to_square] for move in self.board.generate_legal_moves()]
+#         self.action_space[moves] = 1
+#         return self.action_space
+
+#     def get_material_value(self) -> int:
+#         """
+#         Sums up the material balance using Reinfield values
+#         Returns:
+#             The material balance on the board
+#         """
+#         pawns = torch.count_nonzero(self.layer_board[0])
+#         rooks = 5 * torch.count_nonzero(self.layer_board[1])
+#         minor = 3 * torch.count_nonzero(self.layer_board[2:4])
+#         queen = 9 * torch.count_nonzero(self.layer_board[4])
+#         return pawns + rooks + minor + queen
+
+#     def reset(self):
+#         """
+#         Reset the environment
+#         """
+#         self.board = chess.Board.from_board(chess.Board(self.FEN)) if self.FEN else chess.Board()
+#         self.layer_board = torch.zeros_like(self.layer_board)
+#         self.init_layer_board()
+#         self.init_action_space()
