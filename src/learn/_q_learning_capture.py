e4efe9daad5edf32ea83c508fa96c65691b06c05
Valeria Insogna <valinsogna@gmail.com>
2023-07-19 11:50:37 +0200

    keras

--- src/learn/_q_learning_capture.py
+++ src/learn/_q_learning_capture.py
@@ -1,16 +1,13 @@
 import numpy as np
 from chess.pgn import Game
 import pandas as pd
-import torch
-import torch.nn as nn
-import torch.optim as optim
 
 
-class Q_learning_capture(object):
+class Q_learning(object):
 
     def __init__(self, agent, env, memsize=1000):
         """
-        Q-learning object to learn capture chess
+        Reinforce object to learn capture chess
         Args:
             agent: The agent playing the chess game as white
             env: The environment including the python-chess board
@@ -23,21 +20,19 @@ class Q_learning_capture(object):
         self.reward_trace = []
         self.memory = []
         self.sampling_probs = []
-        self.criterion = nn.MSELoss()
-        self.optimizer = optim.SGD(self.agent.parameters(), lr=0.01)
 
     def learn(self, iters=100, c=10):
         """
-        Run the Q-learning algorithm. Play greedy on the final iteration.
+        Run the Q-learning algorithm. Play greedy on the final iter
         Args:
             iters: int
-                number of games to train
+                amount of games to train
             c: int
                 update the network every c games
 
-        Returns:
-            pgn: str
-                pgn string describing the final game
+        Returns: pgn (str)
+            pgn string describing final game
+
         """
         for k in range(iters):
             if k % c == 0:
@@ -59,34 +54,36 @@ class Q_learning_capture(object):
         Args:
             k: int
                 game count, determines epsilon (exploration rate)
-            greedy: bool
-                if True, no exploration is done
+            greedy: Boolean
+                if greedy, no exploration is done
             maxiter: int
-                Maximum number of steps per game
+                Maximum amount of steps per game
 
         Returns:
-            board: chess.Board
-                Final state of the chess board
+
         """
         episode_end = False
         turncount = 0
 
+        # Here we determine the exploration rate. k is divided by 250 to slow down the exploration rate decay.
+        eps = max(0.05, 1 / (1 + (k / 250))) if not greedy else 0.
+
+        # Play a game of chess
         while not episode_end:
-            eps = max(0.05, 1 / (1 + (k / 250))) if not greedy else 0.
-            state = torch.from_numpy(self.env.layer_board).unsqueeze(0).float()
+            state = self.env.layer_board
             explore = np.random.uniform(0, 1) < eps  # determine whether to explore
             if explore:
                 move = self.env.get_random_action()
                 move_from = move.from_square
                 move_to = move.to_square
             else:
-                action_values = self.agent.get_action_values(state)
-                action_values = action_values.view(64, 64)
+                action_values = self.agent.get_action_values(np.expand_dims(state, axis=0))
+                action_values = np.reshape(np.squeeze(action_values), (64, 64))
                 action_space = self.env.project_legal_moves()  # The environment determines which moves are legal
-                action_values = torch.mul(action_values, torch.tensor(action_space))
-                move_from = torch.argmax(action_values).item() // 64
-                move_to = torch.argmax(action_values).item() % 64
-                moves = [x for x in self.env.board.generate_legal_moves() if
+                action_values = np.multiply(action_values, action_space)
+                move_from = np.argmax(action_values, axis=None) // 64
+                move_to = np.argmax(action_values, axis=None) % 64
+                moves = [x for x in self.env.board.generate_legal_moves() if \
                          x.from_square == move_from and x.to_square == move_to]
                 if len(moves) == 0:  # If all legal moves have negative action value, explore.
                     move = self.env.get_random_action()
@@ -96,7 +93,7 @@ class Q_learning_capture(object):
                     move = np.random.choice(moves)  # If there are multiple max-moves, pick a random one.
 
             episode_end, reward = self.env.step(move)
-            new_state = torch.from_numpy(self.env.layer_board).unsqueeze(0).float()
+            new_state = self.env.layer_board
             if len(self.memory) > self.memsize:
                 self.memory.pop(0)
                 self.sampling_probs.pop(0)
@@ -105,7 +102,7 @@ class Q_learning_capture(object):
                 episode_end = True
                 reward = 0
             if episode_end:
-                new_state = torch.zeros_like(new_state)
+                new_state = new_state * 0
             self.memory.append([state, (move_from, move_to), reward, new_state])
             self.sampling_probs.append(1)
 
@@ -122,11 +119,10 @@ class Q_learning_capture(object):
             turncount: int
                 turncount limits the size of the minibatch
 
-        Returns:
-            minibatch: list
-                a mini-batch of experiences
-            indices: list
-                indices of chosen experiences
+        Returns: tuple
+            a mini-batch of experiences (list)
+            indices of chosen experiences
+
         """
         minibatch = []
         memory = self.memory[:-turncount]
@@ -140,20 +136,175 @@ class Q_learning_capture(object):
 
     def update_agent(self, turncount):
         """
-        Update the agent using experience replay. Set the sampling probs with the TD error.
+        Update the agent using experience replay. Set the sampling probs with the td error
         Args:
             turncount: int
-                Amount of turns played. Only sample the memory if there are sufficient samples
+                Amount of turns played. Only sample the memory of there are sufficient samples
         Returns:
-            None
+
         """
         if turncount < len(self.memory):
             minibatch, indices = self.sample_memory(turncount)
-            td_errors = []
-            for sample in minibatch:
-                state, action, reward, new_state = sample
-                q_values = self.agent.get_action_values(state)
-                q_values = q_values.view(64, 64)
-                next_q_values = self.agent.get_action_values(new_state)
-                next_q_values = next_q_values.view(64, 64)
-                target_q_values = q_values.clone().detach
+            td_errors = self.agent.network_update(minibatch)
+            for n, i in enumerate(indices):
+                self.sampling_probs[i] = np.abs(td_errors[n])
+
+# import numpy as np
+# from chess.pgn import Game
+# import pandas as pd
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
+
+
+# class Q_learning_capture(object):
+
+#     def __init__(self, agent, env, memsize=1000):
+#         """
+#         Q-learning object to learn capture chess
+#         Args:
+#             agent: The agent playing the chess game as white
+#             env: The environment including the python-chess board
+#             memsize: maximum amount of games to retain in-memory
+#         """
+#         self.agent = agent
+#         self.env = env
+#         self.memory = []
+#         self.memsize = memsize
+#         self.reward_trace = []
+#         self.memory = []
+#         self.sampling_probs = []
+#         self.criterion = nn.MSELoss()
+#         self.optimizer = optim.SGD(self.agent.parameters(), lr=0.01)
+
+#     def learn(self, iters=100, c=10):
+#         """
+#         Run the Q-learning algorithm. Play greedy on the final iteration.
+#         Args:
+#             iters: int
+#                 number of games to train
+#             c: int
+#                 update the network every c games
+
+#         Returns:
+#             pgn: str
+#                 pgn string describing the final game
+#         """
+#         for k in range(iters):
+#             if k % c == 0:
+#                 print("iter", k)
+#                 self.agent.fix_model()
+#             greedy = True if k == iters - 1 else False
+#             self.env.reset()
+#             self.play_game(k, greedy=greedy)
+
+#         pgn = Game.from_board(self.env.board)
+#         reward_smooth = pd.DataFrame(self.reward_trace)
+#         reward_smooth.rolling(window=10, min_periods=0).mean().plot()
+
+#         return pgn
+
+#     def play_game(self, k, greedy=False, maxiter=25):
+#         """
+#         Play a game of capture chess
+#         Args:
+#             k: int
+#                 game count, determines epsilon (exploration rate)
+#             greedy: bool
+#                 if True, no exploration is done
+#             maxiter: int
+#                 Maximum number of steps per game
+
+#         Returns:
+#             board: chess.Board
+#                 Final state of the chess board
+#         """
+#         episode_end = False
+#         turncount = 0
+
+#         while not episode_end:
+#             eps = max(0.05, 1 / (1 + (k / 250))) if not greedy else 0.
+#             state = torch.from_numpy(self.env.layer_board).unsqueeze(0).float()
+#             explore = np.random.uniform(0, 1) < eps  # determine whether to explore
+#             if explore:
+#                 move = self.env.get_random_action()
+#                 move_from = move.from_square
+#                 move_to = move.to_square
+#             else:
+#                 action_values = self.agent.get_action_values(state)
+#                 action_values = action_values.view(64, 64)
+#                 action_space = self.env.project_legal_moves()  # The environment determines which moves are legal
+#                 action_values = torch.mul(action_values, torch.tensor(action_space))
+#                 move_from = torch.argmax(action_values).item() // 64
+#                 move_to = torch.argmax(action_values).item() % 64
+#                 moves = [x for x in self.env.board.generate_legal_moves() if
+#                          x.from_square == move_from and x.to_square == move_to]
+#                 if len(moves) == 0:  # If all legal moves have negative action value, explore.
+#                     move = self.env.get_random_action()
+#                     move_from = move.from_square
+#                     move_to = move.to_square
+#                 else:
+#                     move = np.random.choice(moves)  # If there are multiple max-moves, pick a random one.
+
+#             episode_end, reward = self.env.step(move)
+#             new_state = torch.from_numpy(self.env.layer_board).unsqueeze(0).float()
+#             if len(self.memory) > self.memsize:
+#                 self.memory.pop(0)
+#                 self.sampling_probs.pop(0)
+#             turncount += 1
+#             if turncount > maxiter:
+#                 episode_end = True
+#                 reward = 0
+#             if episode_end:
+#                 new_state = torch.zeros_like(new_state)
+#             self.memory.append([state, (move_from, move_to), reward, new_state])
+#             self.sampling_probs.append(1)
+
+#             self.reward_trace.append(reward)
+
+#             self.update_agent(turncount)
+
+#         return self.env.board
+
+#     def sample_memory(self, turncount):
+#         """
+#         Get a sample from memory for experience replay
+#         Args:
+#             turncount: int
+#                 turncount limits the size of the minibatch
+
+#         Returns:
+#             minibatch: list
+#                 a mini-batch of experiences
+#             indices: list
+#                 indices of chosen experiences
+#         """
+#         minibatch = []
+#         memory = self.memory[:-turncount]
+#         probs = self.sampling_probs[:-turncount]
+#         sample_probs = [probs[n] / np.sum(probs) for n in range(len(probs))]
+#         indices = np.random.choice(range(len(memory)), min(1028, len(memory)), replace=True, p=sample_probs)
+#         for i in indices:
+#             minibatch.append(memory[i])
+
+#         return minibatch, indices
+
+#     def update_agent(self, turncount):
+#         """
+#         Update the agent using experience replay. Set the sampling probs with the TD error.
+#         Args:
+#             turncount: int
+#                 Amount of turns played. Only sample the memory if there are sufficient samples
+#         Returns:
+#             None
+#         """
+#         if turncount < len(self.memory):
+#             minibatch, indices = self.sample_memory(turncount)
+#             td_errors = []
+#             for sample in minibatch:
+#                 state, action, reward, new_state = sample
+#                 q_values = self.agent.get_action_values(state)
+#                 q_values = q_values.view(64, 64)
+#                 next_q_values = self.agent.get_action_values(new_state)
+#                 next_q_values = next_q_values.view(64, 64)
+#                 target_q_values = q_values.clone().detach
